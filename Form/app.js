41d336d2087bd110a461dfb1268cbff172adc701
John Doe <a.xolod15@mail.ru>
2022-02-18 18:02:19 +0300

    add JSON

--- Form/app.js
+++ Form/app.js
@@ -1,16 +1,31 @@
 const form = document.querySelector('form');
-const input = document.querySelectorAll('input');
+const input = form.querySelectorAll('input');
 const formSubmit = document.querySelector('button');
 const checkbox = document.querySelector('.custom-checkbox');
+let arr = [];
+let key = [];
+
+function OBJ(key, arr) {
+  let result = {};
+  for (let i = 0; i < key.length; i++) {
+      result[key[i]] = arr[i];
+  }
+  return  JSON.stringify(result);
+};
 
 form.addEventListener('change', changeFormHandler);
 function changeFormHandler() {
   console.log(form.checkValidity());
   if (form.checkValidity() === true) {
     formSubmit.addEventListener('click',() => {if (checkbox.checked) {
-        for(let i = 0; i < input.length; i++){
-        input[i].value = '';}
-    } else {alert('поставьте флажок')}
+        input.forEach((el,i) =>{
+          key.push(el.placeholder !== '' ? el.placeholder: `photoPath${i+1}str`);
+          arr.push(el.value);
+          el.value = '';})
+          console.log(arr, key);
+    } else {alert('примите лицензионное соглашение')}
+    console.log(OBJ(key,arr));
     });
   }
 }
+
